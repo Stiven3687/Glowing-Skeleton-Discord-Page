b404a82b51513d4e05882beb2122267f1839f467
Stiven3687 <johanroblox.com@gmail.com>
2022-01-09 17:56:15 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="texto">Discord Server</i>',
+        '<i class="texto">Facebook Fan Page</i>',
+        '<i class="texto">Comunity</i>'
+    ],
+    //stringsElement: '#typed',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    ShowCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
